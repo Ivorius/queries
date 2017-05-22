4d7a0f11c8b11ac464e45686170f523a119317c7
Michael Moravec <mail@majkl578.cz>
2017-05-22 18:59:26 +0200

    Enable strict types

--- tests/src/Mocks/UserQuery.php
+++ tests/src/Mocks/UserQuery.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace LibretteTests\Queries\Mocks;
 
 use Librette\Queries\IQuery;
--- tests/src/OuterQueriesTestCase.phpt
+++ tests/src/OuterQueriesTestCase.phpt
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace LibretteTests\Queries;
 
 use Librette\Queries\CountQuery;
